a312cca6d6e2852aea995363a0b275585ec09094
burhanahmeed <burhanahmeed@gmail.com>
2022-05-29 16:14:29 +0700

    feat: add container component

---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from 'react';
+import classNames from 'classnames';
+
+interface IProps {
+  children: ReactNode;
+}
+
+const Container = (props: IProps) => {
+  const containerClass = classNames({
+    container: true,
+  });
+
+  return <section className={containerClass}>{props.children}</section>;
+};
+
+export default Container;
