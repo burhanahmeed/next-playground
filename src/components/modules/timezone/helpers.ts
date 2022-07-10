34e894655360c12ce0b5e553e762e4335764beba
burhanahmeed <burhanahmeed@gmail.com>
2022-07-10 21:11:53 +0700

    chore: run linter

--- src/components/modules/timezone/helpers.ts
+++ src/components/modules/timezone/helpers.ts
@@ -1,11 +1,15 @@
 import Timezones from '@/components/modules/timezone/data.json';
 
-export const generateHourArray = (defaultTz: string, timezone: string, isPrimary = false) => {
+export const generateHourArray = (
+  defaultTz: string,
+  timezone: string,
+  isPrimary = false
+) => {
   const initial = Array.from(Array(24).keys()).map((e) => {
     return {
       hour: e,
       day: 0,
-    }
+    };
   });
   const tz = Timezones.find((e) => e.value === timezone);
   const defTz = Timezones.find((e) => e.value === defaultTz);
@@ -15,16 +19,25 @@ export const generateHourArray = (defaultTz: string, timezone: string, isPrimary
     const roundedTimeDiff = Number(timeDifference.toFixed(0));
 
     if (timeDifference > 0) {
-      return [...initial.slice(roundedTimeDiff, initial.length), ...initial.slice(0, roundedTimeDiff)];
-    } else {
-      return [...initial.slice(initial.length - roundedTimeDiff, initial.length), ...initial.slice(0, initial.length - roundedTimeDiff)];
+      return [
+        ...initial.slice(roundedTimeDiff, initial.length),
+        ...initial.slice(0, roundedTimeDiff),
+      ];
     }
+    return [
+      ...initial.slice(initial.length - roundedTimeDiff, initial.length),
+      ...initial.slice(0, initial.length - roundedTimeDiff),
+    ];
   }
 
   return initial;
-}
+};
 
-export const getMetaData = (defaultTz: string, timezone: string, isPrimary = false) => {
+export const getMetaData = (
+  defaultTz: string,
+  timezone: string,
+  isPrimary = false
+) => {
   const tz = Timezones.find((e) => e.value === timezone);
   const defTz = Timezones.find((e) => e.value === defaultTz);
   let timeDifference = 0;
@@ -36,4 +49,4 @@ export const getMetaData = (defaultTz: string, timezone: string, isPrimary = fal
     timezone: isPrimary ? defTz : tz,
     difference: isPrimary ? 0 : timeDifference,
   };
-}
+};
