a4972d3ff96b97e162f97a90fe3d920887d78522
burhanahmeed <burhanahmeed@gmail.com>
2022-11-06 09:58:35 +0700

    feat: add app list

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 const Card = (props: IProps) => {
   return (
-    <div className="cursor-pointer rounded-lg bg-gray-700 bg-gradient-to-r p-4 transition duration-300 ease-in-out hover:scale-105 hover:from-transparent hover:to-purple-900">
+    <div className="rounded-lg bg-gray-700 bg-gradient-to-r p-4 transition duration-300 ease-in-out hover:scale-105 hover:from-transparent hover:to-purple-900">
       {props.children}
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
 // UI
 import { Main } from '@/components/templates/Main';
 import { Meta } from '@/components/utils/Meta';
 import Container from '@/components/ui/Container';
 import MainHeader from '@/components/ui/MainHeader';
 import Footer from '@/components/ui/Footer';
-import EmptyList from '@/components/ui/EmptyList';
-// import Card from '@/components/ui/Card';
+// import EmptyList from '@/components/ui/EmptyList';
+// import { Card } from 'antd';
+import Card from '@/components/ui/Card';
 
 const Index = () => {
   return (
@@ -21,10 +23,17 @@ const Index = () => {
     >
       <Container>
         <div className="p-16">
-          {/* <Card>
-            <h3 className="text-white">Lorem ipsum</h3>
-          </Card> */}
-          <EmptyList
+          <Card>
+            <div className="flex items-center justify-between">
+              <h3 className="text-white">Timezone Playground</h3>
+              <Link href={'/timezone'}>
+                <span className="cursor-pointer text-white hover:text-gray-500">
+                  {'> Open <'}
+                </span>
+              </Link>
+            </div>
+          </Card>
+          {/* <EmptyList
             icon={
               <img
                 src="https://www.svgrepo.com/show/271821/dizzy.svg"
@@ -35,7 +44,7 @@ const Index = () => {
               />
             }
             text="Oops, there is no project yet..."
-          />
+          /> */}
         </div>
       </Container>
     </Main>
