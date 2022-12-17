e6a8607468d9fd9fced1130ba267203101eb8ded
burhanahmeed <burhanahmeed@gmail.com>
2022-12-17 20:00:59 +0700

    feat: add google analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,23 @@ import 'antd/dist/antd.css';
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Script
+      strategy="lazyOnload"
+      src="https://www.googletagmanager.com/gtag/js?id=G-Y67T8Y0MTE"
+    />
+    <Script id="google-analytics" strategy="lazyOnload">
+      {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-Y67T8Y0MTE');`}
+    </Script>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
